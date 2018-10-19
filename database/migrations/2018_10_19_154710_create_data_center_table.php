13105960d9257ce06b244fabe34ddee6006cfaf7
crcms <crcms@crcms.cn>
2018-10-19 16:38:03 +0800

    update field description

--- database/migrations/2018_10_19_154710_create_data_center_table.php
+++ database/migrations/2018_10_19_154710_create_data_center_table.php
@@ -16,7 +16,7 @@ class CreateDataCenterTable extends Migration
         Schema::create('data_center', function (Blueprint $table) {
             $table->string('channel', 50)->comment('Channel');
             $table->string('key', 50)->comment('Key');
-            $table->string('type', 20)->comment('Constant type');
+            $table->string('type', 20)->comment('Value type');
             $table->text('value')->nullable()->comment('Value');
             $table->string('remark', 255)->nullable()->comment('Remark');
             $table->primary(['channel', 'key']);
